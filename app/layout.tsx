5cb11cf5d61f616fd1c81166a96363508057b2e7
Saint-Visions <ryan@saintvisiongroup.com>
2025-07-29 22:14:59 -0700

    Emergency commit before evacuation

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={\`\${inter.className} bg-black text-white\`}>
+      <body className={`${inter.className} bg-black text-white`}>
         <main>{children}</main>
       </body>
     </html>
